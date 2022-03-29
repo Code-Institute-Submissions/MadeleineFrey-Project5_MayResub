ba6c312e1bf2be7f160a3b5a1d70e95e70253476
Madde <Madde.frey@gmail.com>
2022-03-29 20:00:07 +0000

    Add boxes app and create models

---
+++ boxes/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    """ Model to store different Categories"""
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=60)
+    sku = models.CharField(max_length=12, null=True, blank=True)
+    description = models.TextField()
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    size = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
