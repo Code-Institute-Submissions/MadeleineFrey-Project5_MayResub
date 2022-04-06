4dc37b734d6d7005785f31e2626f6fe9b1e48526
Madde <Madde.frey@gmail.com>
2022-04-06 13:42:19 +0000

    Set environment variables and add imports to settings.py

--- subscription_box/settings.py
+++ subscription_box/settings.py
@@ -14,6 +14,12 @@ from pathlib import Path
 
 import os
 
+import dj_database_url
+if os.path.isfile("env.py"):
+    import env
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uy(2pt6o87$rx_839$^y=whvg_pd$tu(#us2@3nxrioxa5vs-y'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
