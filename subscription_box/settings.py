c788e79aed096e8c952f132960b1d571c830d1ba
Madde <Madde.frey@gmail.com>
2022-04-06 14:54:34 +0000

    Add required values for deployment to settings.py and Procfile

--- subscription_box/settings.py
+++ subscription_box/settings.py
@@ -20,6 +20,7 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['food-boxes.herokuapp.com', 'localhoast']
 
 
 # Application definition
