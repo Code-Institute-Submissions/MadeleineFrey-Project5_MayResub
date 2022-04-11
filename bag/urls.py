8fb1287b20b729151d43ebbff43de666964ee530
Madde <Madde.frey@gmail.com>
2022-04-11 20:46:18 +0000

    Add, add box to bag functionality

--- bag/urls.py
+++ bag/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_bag, name='bag')
+    path('', views.view_bag, name='bag'),
+    path('add/<box_id>/', views.add_to_bag, name='add_to_bag'),
+
 ]
