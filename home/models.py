df471dd086e78bb8270306afa81d1533075ff37b
Madde <Madde.frey@gmail.com>
2022-04-16 17:45:45 +0000

    Add About site model for about us page

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class AboutUs(models.Model):
+    """ X """
+
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    years = models.DecimalField(max_digits=6, decimal_places=2)
+    profile_image = models.ImageField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
