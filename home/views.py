f363b2ee3c52d6ce22bbb655237d3275dafe1dd5
Madde <Madde.frey@gmail.com>
2022-04-16 17:53:10 +0000

    Connect views and urls to about us page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 
+from .models import AboutUs
 # Create your views here.
 
 def index(request):
     """ A view to return the index/home page """
 
     return render(request, 'home/index.html')
+
+def about_us(request):
+    """ A view to """
+    team = AboutUs.objects.all()
+    
+    context = {
+        'team': team,
+    }
+
+    return render(request, 'home/about_us.html', context)
