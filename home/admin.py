9dfe11f9c2bd922aed44db8cdb485a25d913eea9
Madde <Madde.frey@gmail.com>
2022-04-16 21:42:10 +0000

    Add admin.py for home app

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Team, Location, Contact
 
 # Register your models here.
+class TeamAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+        'role',
+    )
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        'address',
+    )
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'email',
+        'phone_number',
+    )
+
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Contact, ContactAdmin)
+
--- home/models.py
+++ home/models.py
@@ -20,8 +20,9 @@ class Location(models.Model):
         return self.address
 
 class Contact(models.Model):
+    name = models.CharField(max_length=60, null=True, blank=True)
     email = models.CharField(max_length=60)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     
     def __str__(self):
-        return self.email
+        return self.name
