de7a0e2ef5ae4bca7d0796df1e9d06c2ceb739bc
Madde <Madde.frey@gmail.com>
2022-05-21 12:29:17 +0000

    Change Email_backend settings

--- subscription_box/settings.py
+++ subscription_box/settings.py
@@ -201,7 +201,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'box20321@gmail.com'
+    DEFAULT_FROM_EMAIL = 'boxs20321@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
